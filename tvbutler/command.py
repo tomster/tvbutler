8182ba9c4a7f8e02219a026dbad82c2dbc068645
Tom Lazar <tom@tomster.org>
2010-12-05 23:12:25 +0100

    Use logging instead of printing to stdout.

--- tvbutler/command.py
+++ tvbutler/command.py
@@ -4,9 +4,8 @@ from os import path
 from urllib import urlretrieve
 
 from scraper import extract_metadata
-from settings import get_settings
+from settings import settings, log
 from persistence import TVShow, Session
-
 
 parser = OptionParser()
 parser.add_option("-f", "--file", dest="filename",
@@ -15,7 +14,6 @@ parser.add_option("-f", "--file", dest="filename",
 
 
 def main():
-    settings = get_settings()
     session = Session()
     (options, args) = parser.parse_args()
     if options.filename is not None:
@@ -23,15 +21,16 @@ def main():
     else:
         feeds = settings.get('main', 'feeds').split()
     preferred_quality = settings.get('main', 'preferred_quality')
+    log.info("Begin of run.")
     for feed_url in feeds:
         feed = parse(feed_url)
-        print "Checking %s" % feed['feed']['subtitle']
+        log.info("Checking %s" % feed['feed']['subtitle'])
         for entry in feed.entries:
             data = extract_metadata(entry.description)
             try:
                 data['torrent_url'] = entry.enclosures[0]['href']
             except (IndexError, KeyError):
-                print "No torrent found for %(name)s" % data
+                log.warning("No torrent found for %(name)s" % data)
             show = TVShow(**data)
             existing_qualities = session.query(TVShow).filter(TVShow.name==show.name).\
                 filter(TVShow.season==show.season).\
@@ -42,7 +41,7 @@ def main():
             # nothing yet? then add unconditionally
             if existing_qualities.count()==0:
                 session.add(show)
-                print "Added %(name)s %(title)s in %(quality)s" % show.__dict__
+                log.info("Added %(name)s %(title)s in %(quality)s" % show.__dict__)
             # already in preferred quality?
             elif preferred_qualities.count() > 0:
                 continue
@@ -52,17 +51,18 @@ def main():
                 if show.quality != existing_quality.quality:
                     session.delete(existing_quality)
                     session.add(show)
-                    print "Updated %(name)s %(title)s to %(quality)s" % show.__dict__
+                    log.info("Updated %(name)s %(title)s to %(quality)s" % show.__dict__)
     
     torrent_download_dir = path.expanduser(settings.get('main', 'torrent_download_dir'))
-    print "downloading torrents to %s" % torrent_download_dir
+    log.info("downloading torrents to %s" % torrent_download_dir)
     for show in session.query(TVShow).filter(TVShow.status==u'new'):
         torrent_path, result = urlretrieve(show.torrent_url, path.join(torrent_download_dir,
             "%s.torrent" % show.filename))
         if result.type == 'application/x-bittorrent':
             show.status = u'torrent_downloaded'
-            print "Downloading torrent for %(name)s %(title)s in %(quality)s" % show.__dict__
+            log.info("Downloading torrent for %(name)s %(title)s in %(quality)s" % show.__dict__)
         else:
-            print "ERROR: Couldn't download %s" % show.torrent_url
+            log.error("Couldn't download %s" % show.torrent_url)
 
     session.commit()
+    log.info("End of run.")
--- tvbutler/settings.py
+++ tvbutler/settings.py
@@ -1,19 +1,26 @@
+import logging
 from os import mkdir, path
 import ConfigParser
 from shutil import copy
 
 CONFIG_PATH = path.expanduser('~/.tvbutler')
 DB_URL = 'sqlite:///%s' % path.join(CONFIG_PATH, 'database.db')
+log = logging.getLogger('tvbutler')
 
-def get_settings():
-    settings_path = path.join(CONFIG_PATH, 'config')
-    if not path.exists(CONFIG_PATH):
-        mkdir(CONFIG_PATH, 0700)
-        default_settings_path = path.join(path.dirname(path.abspath(__file__)), 'config-sample.ini')
-        copy(default_settings_path, settings_path)
-        print "No feeds specified. Add them to %s and try again." % settings_path
-        exit()
-    config = ConfigParser.ConfigParser()
-    config.read(settings_path)
-    return config
+settings_path = path.join(CONFIG_PATH, 'config')
+if not path.exists(CONFIG_PATH):
+    mkdir(CONFIG_PATH, 0700)
+    default_settings_path = path.join(path.dirname(path.abspath(__file__)), 'config-sample.ini')
+    copy(default_settings_path, settings_path)
+    print "No feeds specified. Add them to %s and try again." % settings_path
+    exit()
+
+settings = ConfigParser.ConfigParser()
+settings.read(settings_path)
+
+handler = logging.FileHandler(path.join(CONFIG_PATH, 'log'))
+formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
+handler.setFormatter(formatter)
+log.addHandler(handler)
+log.setLevel(logging.INFO)
 
