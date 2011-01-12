34f0bca2a6f97025b4b55f83507aa3e1510834c4
Tom Lazar <tom@tomster.org>
2011-01-12 01:16:24 +0100

    Tests for issue #5 https://github.com/tomster/tvbutler/pull/5

--- tvbutler/tests/test_scraper.py
+++ tvbutler/tests/test_scraper.py
@@ -23,3 +23,23 @@ class TestScraper(unittest.TestCase):
         self.assertEqual(data['episode'], u'02')
         self.assertEqual(data['filename'], u'CSI.S11E02.Pool.Shark.720p.HDTV.X264-DIMENSION.mkv')
         self.assertEqual(data['quality'], u'720p')
+
+    def test_episode_extraction_with_colon_in_name(self):
+        data = extract_metadata(u"""Show Name:Law & Order: Whatever; Show Title: Shit: it went down; Season: 12; 
+    Episode: 02; Filename: Law & Order: Whatever.S11E02.Shit:It.Went.Down.X264-DIMENSION.mkv;""")
+        self.assertEqual(data['name'], u'Law & Order: Whatever')
+        self.assertEqual(data['title'], u'Shit: it went down')
+        self.assertEqual(data['season'], u'12')
+        self.assertEqual(data['episode'], u'02')
+        self.assertEqual(data['filename'], u'Law & Order: Whatever.S11E02.Shit:It.Went.Down.X264-DIMENSION.mkv')
+        self.assertEqual(data['quality'], u'sd')
+
+    def test_episode_extraction_with_two_colons_in_name(self):
+        data = extract_metadata(u"""Show Name:Law & Order:: Whatever; Show Title: Shit: it went down; Season: 12; 
+    Episode: 02; Filename: Law & Order: Whatever.S11E02.Shit:It.Went.Down.X264-DIMENSION.mkv;""")
+        self.assertEqual(data['name'], u'Law & Order:: Whatever')
+        self.assertEqual(data['title'], u'Shit: it went down')
+        self.assertEqual(data['season'], u'12')
+        self.assertEqual(data['episode'], u'02')
+        self.assertEqual(data['filename'], u'Law & Order: Whatever.S11E02.Shit:It.Went.Down.X264-DIMENSION.mkv')
+        self.assertEqual(data['quality'], u'sd')
